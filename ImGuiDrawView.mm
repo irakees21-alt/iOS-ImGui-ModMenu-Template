089a6b2f0beacef8eb499a7a24d3bfb79049e3b4
irakees21-alt <irakees21@gmail.com>
2026-09-14 04:48:21 +0200

    Refactor imports and implement silent patching
    Updated imports and added memory patching functions with silent initialization.

--- ImGuiDrawView.mm
+++ ImGuiDrawView.mm
@@ -1,32 +1,33 @@
 #import <Metal/Metal.h>
-#import <MetalKit/MetalKit.h>
 #import <Foundation/Foundation.h>
-//Imgui library
-#import "Esp/CaptainHook.h"
-#import "Esp/ImGuiDrawView.h"
-#import "IMGUI/imgui.h"
-#import "IMGUI/imgui_impl_metal.h"
-#import "IMGUI/Honkai.h"
-//Patch library
-#import "5Toubun/NakanoIchika.h"
-#import "5Toubun/NakanoNino.h"
-#import "5Toubun/NakanoMiku.h"
-#import "5Toubun/NakanoYotsuba.h"
-#import "5Toubun/NakanoItsuki.h"
+#import <UIKit/UIKit.h>
+#import <mach-o/dyld.h>
+
+// استدعاء مكتبة dobby الاحترافية لتعديل الذاكرة وتخطي نظام الحماية
 #import "5Toubun/dobby.h"
 
-#define kWidth  [UIScreen mainScreen].bounds.size.width
-#define kHeight [UIScreen mainScreen].bounds.size.height
-#define kScale [UIScreen mainScreen].scale
+// دالة جلب مسار ذاكرة اللعبة (قاعدة البيانات الأساسية)
+uintptr_t getRealOffset(uintptr_t offset) {
+    return _dyld_get_image_header(0) + offset;
+}
 
-/*
-    Components:
- 
- - Metal: The code leverages the Metal framework for graphics rendering and GPU acceleration.
- - ImGui: The ImGui library is used to create and manage the graphical elements of the application's user interface.
- - Patch Library: Various patching and hooking functions are employed to modify the behavior of the target application dynamically.
- - Touch Event Handling: The code handles touch events to allow user interactions with the GUI.
- 
- Key Features:
- 
- - The `MenDeal` 
+// دالة الحماية والتمويه - تعمل تلقائياً بالخلفية فور تشغيل اللعبة دون إظهار أي منيو
+__attribute__((constructor)) static void initialize_silent_patches() {
+    
+    // الانتظار لثوانٍ معدودة حتى تكتمل ملفات اللعبة في الذاكرة لتفادي الكراش التلقائي
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        // 1. تفعيل الأيم الصغير تلقائياً (Offset: 0x345C504)
+        dobby_dummy_patch_rx((void*)getRealOffset(0x345C504), (uint8_t*)"\x1E\x27\x03\xE0", 4);
+
+        // 2. تفعيل الأيم بوت التلقائي الآمن (Offset: 0x2FCA774)
+        dobby_dummy_patch_rx((void*)getRealOffset(0x2FCA774), (uint8_t*)"\x1E\x27\xF0\x08", 4);
+
+        // 3. تفعيل لون اللاعب الأحمر Chams (Offset: 0x66EEDA0)
+        dobby_dummy_patch_rx((void*)getRealOffset(0x66EEDA0), (uint8_t*)"\x1E\x27\xF0\x00", 4);
+
+        // 4. تفعيل ثبات السلاح التلقائي (Offset: 0x345F640)
+        dobby_dummy_patch_rx((void*)getRealOffset(0x345F640), (uint8_t*)"\xD6\x5F\x03\xC0", 4);
+        
+    });
+}
